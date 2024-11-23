f39ae60aec596e8449e8bd8dc78cb99eaaa85c1f
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-11-23 17:34:51 +0530

    new ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import HowToBuy from "./components/HowToBuy"
+import Navbar from "./components/Navbar"
 
 function App() {
 
 
   return (
     <>
-     <div>
-      <h1>hero</h1>
-     </div>
+     <div className="bg-main blur-md"> </div>
+      <div className=" twoblur">  </div>
+
+      
+    <div className=" structure">
+    <Navbar/>
+      <Hero/>
+      <HowToBuy/>
+      <Footer/>
+     
+    </div>
+   
     </>
   )
 }
